1466ee29cb9063aa02f097f335c8ed1551a7a937
maxi <maxi0361@hotmail.com>
2018-01-21 16:36:44 +0800

    add maven

--- src/home/maxi/Main.java
+++ src/home/maxi/Main.java
@@ -3,6 +3,8 @@ package home.maxi;
 import java.io.File;
 import java.nio.file.Path;
 
+import org.apache.commons.io.FilenameUtils;
+
 public class Main {
 
 	public static void main(String[] args) {
@@ -15,13 +17,23 @@ public class Main {
 
 	private static void listAllNestedDir(File root) {
 		if (root.exists() && root.canRead() && root.isDirectory()) {
-			System.out.println(root.getAbsolutePath());
+			//System.out.println(root.getAbsolutePath());
 			File[] files = root.listFiles();
 			if (files != null) {				
 				for (File f : files) {
 					if (f.isDirectory()) {
 						//System.out.println(f.getAbsolutePath());
 						listAllNestedDir(f);
+					}
+					else{
+						if(f.canRead())
+						{
+							String ext = FilenameUtils.getExtension(f.getName());
+							if(ext.equals("mp3"))
+							{
+								System.out.println(f.getAbsolutePath());
+							}
+						}
 					}
 				}
 			}
